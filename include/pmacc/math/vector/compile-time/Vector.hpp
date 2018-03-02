4bd94a6a68de2af7e8859b75418145648aa5cba8
Sergei Bastrakov <sergey.bastrakov@gmail.com>
2018-03-02 14:25:49 +0100

    Replace boost::mpl with boost::mp11 in compile-time vector.

--- include/pmacc/math/vector/compile-time/Vector.hpp
+++ include/pmacc/math/vector/compile-time/Vector.hpp
@@ -21,23 +21,17 @@
 
 #pragma once
 
-#include <boost/mpl/size.hpp>
-#include <boost/mpl/at.hpp>
-#include <boost/mpl/vector.hpp>
-#include <boost/mpl/integral_c.hpp>
-#include <boost/mpl/aux_/na.hpp>
-#include <boost/mpl/plus.hpp>
-#include <boost/mpl/min_max.hpp>
-#include <boost/mpl/times.hpp>
-#include <boost/mp11/integral.hpp>
-#include <boost/mpl/apply.hpp>
-#include <boost/mpl/accumulate.hpp>
-#include <boost/mpl/less.hpp>
-#include <boost/mp11/utility.hpp>
 #include "pmacc/math/Vector.hpp"
 #include "pmacc/types.hpp"
 
+#include <boost/mp11/algorithm.hpp>
+#include <boost/mp11/function.hpp>
+#include <boost/mp11/integral.hpp>
+#include <boost/mp11/list.hpp>
+#include <boost/mp11/utility.hpp>
+
 #include <stdint.h>
+#include <type_traits>
 
 namespace pmacc
 {
@@ -46,7 +40,6 @@ namespace math
 namespace CT
 {
 
-namespace mpl = boost::mpl;
 namespace mp11 = boost::mp11;
 
 namespace detail
@@ -96,6 +89,8 @@ struct VectorFromCT<3>
     }
 };
 
+struct VectorParameterPlaceholder {};
+
 template<typename Arg0>
 struct TypeSelector
 {
@@ -104,40 +99,70 @@ struct TypeSelector
 
 /** get integral type*/
 template<typename T, T value>
-struct TypeSelector<mpl::integral_c<T, value > >
+struct TypeSelector<std::integral_constant<T, value > >
 {
     typedef T type;
 };
 
 template<>
-struct TypeSelector<mpl::na>
+struct TypeSelector<VectorParameterPlaceholder>
 {
     typedef mp11::mp_int<0> type;
 };
 
 }
 
-namespace mpl = boost::mpl;
 namespace mp11 = boost::mp11;
 
-template<typename Arg0 = mpl::na,
-typename Arg1 = mpl::na,
-typename Arg2 = mpl::na>
+
+template<
+    typename Arg0 = detail::VectorParameterPlaceholder,
+    typename Arg1 = detail::VectorParameterPlaceholder,
+    typename Arg2 = detail::VectorParameterPlaceholder
+>
 struct Vector
 {
     typedef Arg0 x;
     typedef Arg1 y;
     typedef Arg2 z;
 
-    typedef mpl::vector<x, y, z> mplVector;
+    using mplVector = mp11::mp_if<
+        mp11::mp_same<
+            x,
+            detail::VectorParameterPlaceholder
+        >,
+        mp11::mp_list<>,
+        mp11::mp_if<
+            mp11::mp_same<
+                y,
+                detail::VectorParameterPlaceholder
+            >,
+            mp11::mp_list< x >,
+            mp11::mp_if<
+                mp11::mp_same <
+                    z,
+                    detail::VectorParameterPlaceholder
+                >,
+                mp11::mp_list<
+                    x,
+                    y
+                >,
+                mp11::mp_list<
+                    x,
+                    y,
+                    z
+                >
+            >
+        >
+    >;
 
     template<int element>
     struct at
     {
-        typedef typename mpl::at_c<mplVector, element>::type type;
+        using type = mp11::mp_at_c<mplVector, element>;
     };
 
-    static constexpr int dim = mpl::size<mplVector >::type::value;
+    static constexpr int dim = mp11::mp_size<mplVector >::value;
 
     typedef typename detail::TypeSelector<x>::type type;
     typedef Vector<x, y, z> This;
@@ -171,89 +196,176 @@ struct Vector
 
 //________________________OperatorBase____________________________
 
-template<typename Lhs, typename Rhs, typename T_BinaryOperator>
+template<
+    typename Lhs,
+    typename Rhs,
+    typename T_BinaryOperator
+>
 struct applyOperator
 {
-    typedef typename applyOperator<typename Lhs::vector_type,
-    typename Rhs::vector_type, T_BinaryOperator>::type type;
+    using type = typename applyOperator<
+        typename Lhs::vector_type,
+        typename Rhs::vector_type,
+        T_BinaryOperator
+    >::type;
 };
 
-template<typename T_TypeA,
-typename T_TypeB,
-typename T_BinaryOperator>
-struct applyOperator<CT::Vector<T_TypeA>, CT::Vector<T_TypeB>, T_BinaryOperator>
+template<
+    typename T_TypeA,
+    typename T_TypeB,
+    typename T_BinaryOperator
+>
+struct applyOperator<
+    CT::Vector< T_TypeA >,
+    CT::Vector< T_TypeB >,
+    T_BinaryOperator
+>
 {
-    typedef typename mpl::apply<T_BinaryOperator, T_TypeA, T_TypeB>::type OpResult;
-    typedef CT::Vector<OpResult> type;
+    using OpResult = T_BinaryOperator<
+        T_TypeA,
+        T_TypeB
+    >;
+    using type = CT::Vector< OpResult >;
 };
 
-template<typename T_TypeA0, typename T_TypeA1,
-typename T_TypeB0, typename T_TypeB1,
-typename T_BinaryOperator>
-struct applyOperator<CT::Vector<T_TypeA0, T_TypeA1>,
-CT::Vector<T_TypeB0, T_TypeB1>,
-T_BinaryOperator>
+template<
+    typename T_TypeA0,
+    typename T_TypeA1,
+    typename T_TypeB0,
+    typename T_TypeB1,
+    typename T_BinaryOperator
+>
+struct applyOperator<
+    CT::Vector<
+        T_TypeA0,
+        T_TypeA1
+    >,
+    CT::Vector<
+        T_TypeB0,
+        T_TypeB1
+    >,
+    T_BinaryOperator
+>
 {
-    typedef typename mpl::apply<T_BinaryOperator, T_TypeA0, T_TypeB0>::type OpResult0;
-    typedef typename mpl::apply<T_BinaryOperator, T_TypeA1, T_TypeB1>::type OpResult1;
-    typedef CT::Vector<OpResult0, OpResult1> type;
+    using OpResult0 = T_BinaryOperator<
+        T_TypeA0,
+        T_TypeB0
+    >;
+    using OpResult1 = T_BinaryOperator<
+        T_TypeA1,
+        T_TypeB1
+    >;
+    using type = CT::Vector<
+        OpResult0,
+        OpResult1
+    >;
 };
 
-template<typename T_TypeA0, typename T_TypeA1, typename T_TypeA2,
-typename T_TypeB0, typename T_TypeB1, typename T_TypeB2,
-typename T_BinaryOperator>
-struct applyOperator<CT::Vector<T_TypeA0, T_TypeA1, T_TypeA2>,
-CT::Vector<T_TypeB0, T_TypeB1, T_TypeB2>,
-T_BinaryOperator>
+template<
+    typename T_TypeA0,
+    typename T_TypeA1,
+    typename T_TypeA2,
+    typename T_TypeB0,
+    typename T_TypeB1,
+    typename T_TypeB2,
+    typename T_BinaryOperator
+>
+struct applyOperator<
+    CT::Vector<
+        T_TypeA0,
+        T_TypeA1,
+        T_TypeA2
+    >,
+    CT::Vector<
+        T_TypeB0,
+        T_TypeB1,
+        T_TypeB2
+    >,
+    T_BinaryOperator
+>
 {
-    typedef typename mpl::apply<T_BinaryOperator, T_TypeA0, T_TypeB0>::type OpResult0;
-    typedef typename mpl::apply<T_BinaryOperator, T_TypeA1, T_TypeB1>::type OpResult1;
-    typedef typename mpl::apply<T_BinaryOperator, T_TypeA2, T_TypeB2>::type OpResult2;
-    typedef CT::Vector<OpResult0, OpResult1, OpResult2> type;
+    using OpResult0 = T_BinaryOperator<
+        T_TypeA0,
+        T_TypeB0
+    >;
+    using OpResult1 = T_BinaryOperator<
+        T_TypeA1,
+        T_TypeB1
+    >;
+    using OpResult2 = T_BinaryOperator<
+        T_TypeA2,
+        T_TypeB2
+    >;
+    using type = CT::Vector<
+        OpResult0,
+        OpResult1,
+        OpResult2
+    >;
 };
 
 //________________________A D D____________________________
 
-template<typename Lhs, typename Rhs>
+template<
+    typename Lhs,
+    typename Rhs
+>
 struct add
 {
-    typedef typename applyOperator<
-    typename Lhs::vector_type,
-    typename Rhs::vector_type,
-    mpl::plus<mpl::_1, mpl::_2> >::type type;
+    using type = applyOperator<
+        typename Lhs::vector_type,
+        typename Rhs::vector_type,
+        mp11::mp_plus
+    >::type;
 };
 
 //________________________M U L____________________________
 
-template<typename Lhs, typename Rhs>
+template<
+    typename Lhs,
+    typename Rhs
+>
 struct mul
 {
-    typedef typename applyOperator<
-    typename Lhs::vector_type,
-    typename Rhs::vector_type,
-    mpl::times<mpl::_1, mpl::_2> >::type type;
+    //typedef typename applyOperator<
+    //typename Lhs::vector_type,
+    //typename Rhs::vector_type,
+    //mpl::times<mpl::_1, mpl::_2> >::type type;
+    ///! Need to replace plus with times
+    using type = applyOperator<
+        typename Lhs::vector_type,
+        typename Rhs::vector_type,
+        mp11::mp_plus
+    >::type;
 };
 
 //________________________M A X____________________________
 
-template<typename Lhs, typename Rhs>
+template<
+    typename Lhs,
+    typename Rhs
+>
 struct max
 {
-    typedef typename applyOperator<
-    typename Lhs::vector_type,
-    typename Rhs::vector_type,
-    mpl::max<mpl::_1, mpl::_2> >::type type;
+    using type = applyOperator<
+        typename Lhs::vector_type,
+        typename Rhs::vector_type,
+        mp11::mp_max
+    >::type;
 };
 
 //________________________M I N____________________________
 
-template<typename Lhs, typename Rhs = void>
+template<
+    typename Lhs,
+    typename Rhs = void
+>
 struct min
 {
-    typedef typename applyOperator<
-    typename Lhs::vector_type,
-    typename Rhs::vector_type,
-    mpl::min<mpl::_1, mpl::_2> >::type type;
+    using type = applyOperator<
+        typename Lhs::vector_type,
+        typename Rhs::vector_type,
+        mp11::mp_min
+    >::type;
 };
 
 template<typename T_Vec>
@@ -262,14 +374,11 @@ struct min<
     void
 >
 {
-    typedef typename mpl::accumulate<
+    using type = applyOperator<
         typename T_Vec::mplVector,
         typename T_Vec::x,
-        mpl::min<
-            mpl::_1,
-            mpl::_2
-        >
-    >::type type;
+        mp11::mp_min
+    >::type;
 };
 
 //________________________D O T____________________________
@@ -277,26 +386,55 @@ struct min<
 template<typename Lhs, typename Rhs>
 struct dot
 {
-    typedef typename mul<Lhs, Rhs>::type MulResult;
+    /*typedef typename mul<Lhs, Rhs>::type MulResult;
     typedef typename mpl::accumulate<
-    typename MulResult::mplVector,
-    mp11::mp_int<0>,
-    mpl::plus<mpl::_1, mpl::_2>
-    >::type type;
+        typename MulResult::mplVector,
+        mp11::mp_int<0>,
+        mp11::mp_plus<
+            mpl::_1,
+            mpl::_2
+        >
+    >::type type;*/
+    ///! Need to replace
+    using type = applyOperator<
+        typename Lhs::vector_type,
+        typename Rhs::vector_type,
+        mp11::mp_plus
+    >::type;
 };
 
 //________________________V O L U M E____________________________
 
-template<typename T_Vec>
+//template<typename T_Vec>
+//struct volume
+//{
+//    typedef typename mpl::accumulate<
+//        typename T_Vec::mplVector,
+//        mpl::int_<1>,
+//        mpl::times<mpl::_1, mpl::_2>
+//    >::type type;
+//};
+
+template< typename T_Vec >
 struct volume
 {
-    typedef typename mpl::accumulate<
-        typename T_Vec::mplVector,
-        ///mp11::mp_int<1>,
-        mpl::int_<1>,
-        mpl::times<mpl::_1, mpl::_2>
-    >::type type;
+private:
+    template< typename T_Arg >
+    struct ToInt {
+        static constexpr int value = T_Arg::value;
+    };
+    template< >
+    struct ToInt< VectorParameterPlaceholder > {
+        static constexpr int value = 1;
+    };
+public:
+    using type = /*bmp11::mp_int<
+        ToInt< typename At< T_Vec::mplVector, bmp11::mp_int< 0 > >::type >::value *
+        ToInt< typename At< T_Vec::mplVector, bmp11::mp_int< 1 > >::type >::value *
+        ToInt< typename At< T_Vec::mplVector, bmp11::mp_int< 2 > >::type >::value
+    >;*/ bmp11::mp_int<10>;
 };
+
 
 //________________________S H R I N K T O________________________
 
@@ -315,21 +453,33 @@ template<typename T_Vec>
 struct shrinkTo<T_Vec, DIM3>
 {
     typedef T_Vec Vec;
-    typedef CT::Vector<typename Vec::x, typename Vec::y, typename Vec::z> type;
+    using type = CT::Vector<
+        typename Vec::x,
+        typename Vec::y,
+        typename Vec::z
+    >;
 };
 
 template<typename T_Vec>
 struct shrinkTo<T_Vec, DIM2>
 {
     typedef T_Vec Vec;
-    typedef CT::Vector<typename Vec::x, typename Vec::y, mpl::na> type;
+    using type = CT::Vector<
+        typename Vec::x,
+        typename Vec::y,
+        VectorParameterPlaceholder
+    >;
 };
 
 template<typename T_Vec>
 struct shrinkTo<T_Vec, DIM1>
 {
     typedef T_Vec Vec;
-    typedef CT::Vector<typename Vec::x, mpl::na, mpl::na> type;
+    using type = CT::Vector<
+        typename Vec::x,
+        VectorParameterPlaceholder,
+        VectorParameterPlaceholder
+    >;
 };
 
 //________________________A S S I G N________________________
@@ -339,26 +489,26 @@ struct shrinkTo<T_Vec, DIM1>
  * defines a public type as result
  *
  * @tparam T_Vec math::CT::Vector which should be changed
- * @tparam T_ComponentPos number of component to changed (type must be bmpl::integral_c<anyType,X>)
+ * @tparam T_ComponentPos number of component to changed (type must be std::integral_constant<anyType,X>)
  * @tparam T_Value new value
  */
 template<typename T_Vec, typename T_ComponentPos, typename T_Value>
 struct Assign;
 
 template<typename T_Value, typename T_0, typename T_1, typename T_2, typename T_IntegralType>
-struct Assign<pmacc::math::CT::Vector<T_0, T_1, T_2>, bmpl::integral_c<T_IntegralType,0> , T_Value>
+struct Assign<pmacc::math::CT::Vector<T_0, T_1, T_2>, std::integral_constant<T_IntegralType,0> , T_Value>
 {
     typedef pmacc::math::CT::Vector<T_Value, T_1, T_2> type;
 };
 
 template<typename T_Value, typename T_0, typename T_1, typename T_2, typename T_IntegralType>
-struct Assign<pmacc::math::CT::Vector<T_0, T_1, T_2>, bmpl::integral_c<T_IntegralType,1>, T_Value>
+struct Assign<pmacc::math::CT::Vector<T_0, T_1, T_2>, std::integral_constant<T_IntegralType,1>, T_Value>
 {
     typedef pmacc::math::CT::Vector<T_0, T_Value, T_2> type;
 };
 
 template<typename T_Value, typename T_0, typename T_1, typename T_2, typename T_IntegralType>
-struct Assign<pmacc::math::CT::Vector<T_0, T_1, T_2>, bmpl::integral_c<T_IntegralType,2>, T_Value>
+struct Assign<pmacc::math::CT::Vector<T_0, T_1, T_2>, std::integral_constant<T_IntegralType,2>, T_Value>
 {
     typedef pmacc::math::CT::Vector<T_0, T_1, T_Value> type;
 };
@@ -370,15 +520,15 @@ struct Assign<pmacc::math::CT::Vector<T_0, T_1, T_2>, bmpl::integral_c<T_Integra
  * defines a public type as result
  *
  * @tparam T_Vec math::CT::Vector which should be changed
- * @tparam T_ComponentPos number of component to changed (type must be bmpl::integral_c<anyType,X>)
+ * @tparam T_ComponentPos number of component to changed (type must be std::integral_constant<anyType,X>)
  * @tparam T_Value new value
  */
 template<typename T_Vec, typename T_ComponentPos, typename T_Value>
 struct AssignIfInRange
 {
-    typedef bmpl::integral_c<size_t,T_Vec::dim> VectorDim;
+    typedef bmp11::mp_size_t<T_Vec::dim> VectorDim;
     typedef bmp11::mp_if<
-        bmpl::less<T_ComponentPos, VectorDim>,
+        bmp11::mp_less<T_ComponentPos, VectorDim>,
         typename pmacc::math::CT::Assign<T_Vec,T_ComponentPos,T_Value>::type,
         T_Vec
     > type;
@@ -393,10 +543,16 @@ struct AssignIfInRange
  * @tparam T_Vec input CT::Vector
  * @tparam T_idx integral index of the component
  */
-template<typename T_Vec,size_t T_idx>
+template<
+    typename T_Vec,
+    size_t T_idx
+>
 struct At_c
 {
-    typedef typename mpl::at_c<typename T_Vec::mplVector,T_idx>::type type;
+    using type = mp11::mp_at_c<
+        typename T_Vec::mplVector,
+        T_idx
+    >;
 };
 
 //________________________At____________________________
@@ -411,7 +567,10 @@ struct At_c
 template<typename T_Vec, typename T_Idx>
 struct At
 {
-    typedef typename mpl::at<typename T_Vec::mplVector,T_Idx>::type type;
+    using type = mp11::mp_at<
+        typename T_Vec::mplVector,
+        T_Idx
+    >::type;
 };
 
 //________________________make_Vector___________________
